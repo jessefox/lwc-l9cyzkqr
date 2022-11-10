cac7388b8ea6990b7cd9370df0061ef485d8c17d
jessefox <1302251+undefined@users.noreply.github.com>
2022-11-10 21:20:55 +0000

    Backup

--- src/paymentPlan.js
+++ src/paymentPlan.js
@@ -5,5 +5,12 @@ export default class PaymentPlan extends LightningElement {
   downPayment = 0;;
   firstPayDate = new Date().toISOString();
   remainingAmount;
+  frequencyOptions = [
+    { label: 'Weekly', value: 'Weekly' },
+    { label: 'Bi-Weekly', value: 'Bi-Weekly' },
+    { label: 'Monthly', value: 'Monthly' },
+    { label: 'Bi-Monthly', value: 'Bi-Monthly' },
+    { label: 'Day of Month', value: 'Day of Month' }
+  ]
 
 }
